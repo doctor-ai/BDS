277e7fd168b9e099259b8ed5019f7de68640f7a3
sanjay1906 <sanjaythakur1906@gmail.com>
2019-10-02 17:44:01 +0530

    somechange

--- portal/src/Pages/Home/layout.js
+++ portal/src/Pages/Home/layout.js
@@ -1,9 +1,28 @@
-import React, { Component } from "react";
-import { withStyles, Typography, Grid, Container, Button, Card, CardContent } from "@material-ui/core";
+import React, { Component } from 'react';
+import {
+  withStyles,
+  Typography,
+  Grid,
+  Container,
+  CardActions,
+  CardMedia,
+  Button,
+  Card,
+  CardContent,
+  Avatar
+} from '@material-ui/core';
 import SingleBedIcon from '@material-ui/icons/SingleBed';
-import style from "./style";
-import { Header } from "Components";
-import { lineHeight } from "@material-ui/system";
+import style from './style';
+import { Header } from 'Components';
+import { Link as RouterLink } from 'react-router-dom';
+
+const cards = [1, 2, 3];
+
+const user = {
+  name: 'Sunny Lione',
+  avatar: '/images/sunny.jpg',
+  bio: 'kishan'
+};
 
 class Layout extends Component {
   render() {
@@ -11,132 +30,233 @@ class Layout extends Component {
     return (
       <div className={classes.header}>
         <Header />
-        <Container maxWidth="lg" className={classes.Container}  >
+        <Container maxWidth='lg' className={classes.Container}>
           <Grid container spacing={4}>
             <Grid item xs={12} md={8} lg={8}>
-              <Typography variant="h3" className={classes.headertypo}>
-                Top Class <span style={{ color: "#1e90ff" }}>Health </span>
+              <Typography variant='h3' className={classes.headertypo}>
+                Top Class <span style={{ color: '#1e90ff' }}>Health </span>
                 <br />
-                <span style={{ color: "#1e90ff" }}>Care</span> Hospital
+                <span style={{ color: '#1e90ff' }}>Care</span> Hospital
               </Typography>
               <p className={classes.paragraph}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book.
               </p>
-              <Button variant="contained" color="primary" className={classes.button}>
+              <Button
+                variant='contained'
+                color='primary'
+                className={classes.button}
+              >
                 Know More
-      </Button>
+              </Button>
             </Grid>
             <Grid item xs={12} md={4} lg={4}>
               <div className={classes.imagecontent}>
-                <img src="/images/dr.jpg" alt="headerimage" className={classes.image} />
+                <img
+                  src='/images/dr.jpg'
+                  alt='headerimage'
+                  className={classes.image}
+                />
               </div>
             </Grid>
           </Grid>
           {/* second Grid */}
-          <Container maxWidth="lg" className={classes.work}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} md={4} lg={4}>
-                <Card>
-                  <div className={classes.cardcontent}>
-                    <CardContent>
-                      <div className={classes.cardcontent}>
-                        <CardContent >
-                          <SingleBedIcon className={classes.icon} />
-                          <Typography variant="h6">
-                            Our 100+ Stretcher
-                    </Typography>
-                          <p>
-                            A hospital is health care institution providing patient treatment with specialized medical.
-                    </p>
-                          <a href="#" >
-                            <Button className={classes.btn}>Know More
-                     </Button>
-                          </a>
-                        </CardContent>
-                      </div>
-                    </CardContent>
-                  </div>
+          <Container className={classes.main} maxWidth='lg'>
+            <Grid container spacing={4}>
+              <Grid item xs={12} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.CardMedia}
+                    component='img'
+                    alt='stretcher '
+                    image='images/stretcher.svg'
+                    title=' stretcher'
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant='h5'>Our 100+ Stretcher</Typography>
+                    <div>
+                      <p>
+                        A hospital is health care institution providing patient
+                        treatment with specialized medical.{' '}
+                      </p>
+                    </div>
+                  </CardContent>
+                  <CardActions>
+                    <Button className={classes.button}>Read More</Button>
+                  </CardActions>
                 </Card>
               </Grid>
-              <Grid item xs={12} md={4} lg={4}>
-                <Card>
-                  <div className={classes.cardcontent}>
-                    <CardContent>
-                      <div className={classes.cardcontent}>
-                        <CardContent >
-                          <SingleBedIcon className={classes.icon} />
-                          <Typography variant="h6">
-                            High Tech  Aqiupment
-                    </Typography>
-                          <p>
-                            e are 10 sparkling minds exporting human communication via digital assets.,
-                    </p>
-                          <a href="#" >
-                            <Button className={classes.btn}>Know More
-                     </Button>
-                          </a>
-                        </CardContent>
-                      </div>
-                    </CardContent>
-                  </div>
+              <Grid item xs={12} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.CardMedia}
+                    component='img'
+                    alt='ambulance '
+                    image='images/ambulance.svg'
+                    title=' ambulance'
+                  />
+
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant='h5'>24 Hours Services</Typography>
+                    <div>
+                      <p>
+                        A teaching hospital combines assitance to provide with
+                        teaching to medical stuent and nurse.
+                      </p>
+                    </div>
+                  </CardContent>
+                  <CardActions>
+                    <Button className={classes.button}> Read More</Button>
+                  </CardActions>
                 </Card>
               </Grid>
-              <Grid item xs={12} md={4} lg={4}>
-                <Card>
-                  <div className={classes.cardcontent}>
-                    <CardContent>
-                      <div className={classes.cardcontent}>
-                        <CardContent >
-                          <SingleBedIcon className={classes.icon} />
-                          <Typography variant="h6">
-                            24 Hours Services
-                    </Typography>
-                          <p>
-                            Some hospitals have outpaintent departments and some have chronic treatment unit.
-                    </p>
-                          <a href="#" >
-                            <Button className={classes.btn}>Know More
-                     </Button>
-                          </a>
-                        </CardContent>
-                      </div>
-                    </CardContent>
-                  </div>
+              <Grid item xs={12} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.CardMedia}
+                    component='img'
+                    alt='protection '
+                    image='images/protection.svg'
+                    title=' protection'
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography variant='h5'>High Tech Aqiupment</Typography>
+                    <div>
+                      <p>
+                        {' '}
+                        Some hospitals have outpaintent departments and some
+                        have chronic treatment unit.
+                      </p>
+                    </div>
+                  </CardContent>
+                  <CardActions>
+                    <Button className={classes.button}>Read More</Button>
+                  </CardActions>
                 </Card>
               </Grid>
             </Grid>
           </Container>
         </Container>
-        <Container maxWidth="lg" className={classes.aboutus}>
-          <Typography variant="h4" className={classes.typo}>
+        <Container maxWidth='lg' className={classes.aboutus}>
+          <Typography variant='h4' className={classes.typoabout}>
             --About Us
           </Typography>
           <Grid container spacing={4}>
             <Grid item xs={12} md={6} lg={6}>
               <div className={classes.imagesection}>
-                <img src="/images/header.jpg" alt="headerimage" className={classes.image} />
+                <img
+                  src='/images/header.jpg'
+                  alt='headerimage'
+                  className={classes.image}
+                />
               </div>
             </Grid>
             <Grid item xs={12} md={6} lg={6}>
               <div className={classes.aboutUsTypo}>
-                <Typography variant="h5" style={{marginBottom:"0.4rem"}}>
+                <Typography variant='h5' style={{ marginBottom: '0.4rem' }}>
                   Dr.Robert Sadanana And Team
-                  </Typography>
-                <div style={{ color: "gray", fontFamily: "Roboto", fontSize: "1rem",marginBottom:"2rem"}}>
+                </Typography>
+                <div
+                  style={{
+                    color: 'gray',
+                    fontFamily: 'Roboto',
+                    fontSize: '1rem',
+                    marginBottom: '2rem'
+                  }}
+                >
                   Senior Doctor,SLK hospital
-                  </div>
+                </div>
                 <p className={classes.description}>
-                  We are 10 sparkling minds exporting human communication via digital assets. Our agency is based in London,
-                   so we are pretty fancy looking. Also we put maximum effort to achieve set goals and we make sure our client got what he
+                  We are 10 sparkling minds exporting human communication via
+                  digital assets. Our agency is based in London, so we are
+                  pretty fancy looking. Also we put maximum effort to achieve
+                  set goals and we make sure our client got what he
                 </p>
-                <Button variant="contained" color="primary" style={{marginTop:"1rem"}}>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  style={{ marginTop: '1rem' }}
+                >
                   Details
-               </Button>
+                </Button>
               </div>
             </Grid>
           </Grid>
         </Container>
+        <Container maxWidth='lg' className={classes.aboutus}>
+          <Typography variant='h4' className={classes.typoprinci}>
+            --Our principles
+          </Typography>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6} lg={6}>
+              <div className={classes.aboutUsTypo}>
+                <Typography variant='h5' style={{ marginBottom: '3.5rem' }}>
+                  What we do
+                </Typography>
 
+                <p className={classes.description}>
+                  We are 10 sparkling minds exporting human communication via
+                  digital assets. Our agency is based in London, so we are
+                  pretty fancy looking. Also we put maximum effort to achieve
+                  set goals and we make sure our client got what he
+                </p>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  style={{ marginTop: '1rem' }}
+                >
+                  Details
+                </Button>
+              </div>
+            </Grid>
+            <Grid item xs={12} md={6} lg={6}>
+              <div className={classes.imagesection}>
+                <img
+                  src='/images/het.webp'
+                  alt='headerimage'
+                  className={classes.image}
+                />
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
+        <Container className={classes.cardGrid} maxWidth='lg'>
+          <Typography variant='h4' className={classes.typotesti}>
+            Testimonials
+          </Typography>
+
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <Avatar
+                    alt='Person'
+                    className={classes.avatar}
+                    component={RouterLink}
+                    src={user.avatar}
+                    // to='/settings'
+                  />
+                  <Typography className={classes.name} variant='h5'>
+                    {user.name}
+                  </Typography>
+                  <Typography className={classes.specification} variant='1px'>
+                    {user.bio}
+                  </Typography>
+                  <CardContent className={classes.cardContent}>
+                    <Typography className={classes.aboutas}>
+                      We are 10 sparkling minds exporting human communication
+                      via digital assets. Our agency is based in London, so we
+                      are pretty fancy
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </div>
     );
   }
--- portal/src/Pages/Home/style.js
+++ portal/src/Pages/Home/style.js
@@ -1,75 +1,140 @@
-import { border } from "@material-ui/system";
-
 const style = theme => ({
-  headertypo:{
-    fontFamily:"Roboto",
-    lineHeight:"3.5rem",
-    fontWeight:"400"
+  headertypo: {
+    fontFamily: 'Roboto',
+    lineHeight: '3.5rem',
+    fontWeight: '400'
   },
-  button:{
-    color:"white"
+  button: {
+    color: 'white'
   },
-  paragraph:{
-    color:"grey",
-    textAlign:"justify"
-  },  
-  Container:{
-    marginTop:"5rem"
+  paragraph: {
+    color: 'grey',
+    textAlign: 'justify'
   },
-  imagecontent:{   
-    height:"350px",
-    width:"auto",
-    position:"relative",
-    clipPath: "polygon(35% 0%, 100% 0, 100% 100%, 25% 100%, 0 46%)",
+  Container: {
+    marginTop: '5rem'
   },
-  image:{
-    height:"100%",
-    width:"100%",
-    position:"absolute",
+  imagecontent: {
+    height: '350px',
+    width: 'auto',
+    position: 'relative',
+    clipPath: 'polygon(35% 0%, 100% 0, 100% 100%, 25% 100%, 0 46%)'
   },
- icon:{
-  //  display:"inline",
-   fontSize:"2rem",
-   marginBottom:"10px",
-   textAlign:"left"
+  image: {
+    height: '100%',
+    width: '100%',
+    position: 'absolute'
   },
-  work:{
-    marginTop:"3rem",
-    width:"100%",
-    margin:0,
-    padding:0,
+  icon: {
+    //  display:"inline",
+    fontSize: '2rem',
+    marginBottom: '10px',
+    textAlign: 'left'
   },
-  cardcontent:{  
-    textAlign:"Left",
-    "&:hover": {
-      backgroundColor: "#1e90ff",
-      color:"white",
+  work: {
+    marginTop: '3rem',
+    width: '100%',
+    margin: 0,
+    padding: 0
+  },
+  cardcontent: {
+    textAlign: 'Left',
+    '&:hover': {
+      backgroundColor: '#1e90ff',
+      color: 'white'
     }
   },
-  btn:{
-    color:"grey",
+  btn: {
+    color: 'grey'
   },
-  aboutus:{
-    marginTop:"2rem",
+  aboutus: {
+    marginTop: '2rem'
   },
-  typo:{
-    fontFamily:"Operator mono",
-    marginBottom:theme.spacing() * 5,
+  typoabout: {
+    fontFamily: 'Operator mono',
+    marginBottom: theme.spacing() * 5
   },
-  imagesection:{
-    height:"350px",
-    width:"auto",
-    position:"relative",
+  typoprinci: {
+    fontFamily: 'Operator mono',
+    marginBottom: theme.spacing() * 5,
+    textAlign: 'end'
   },
-  aboutUsTypo:
-  {
-    padding:"2rem"
+  typotesti: {
+    fontFamily: 'Operator mono',
+    marginBottom: theme.spacing() * 5,
+    textAlign: 'center'
   },
-  description:
-  {
-    textAlign:"justify",
-    color:"gray"
+  imagesection: {
+    height: '350px',
+    width: 'auto',
+    position: 'relative'
+  },
+  aboutUsTypo: {
+    padding: '2rem'
+  },
+  description: {
+    textAlign: 'justify',
+    color: 'gray'
+  },
+
+  //sanjay
+  container: {
+    display: 'flex',
+    alignItems: 'center',
+    flex: 1,
+    flexDirection: 'column',
+    color: '#FAFCFE'
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#F8F8F8',
+    boxShadow: 'none'
+  },
+  cardMedia: {
+    paddingTop: '56.25%'
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: '50'
+  },
+  name: {
+    marginTop: theme.spacing(1)
+  },
+  specification: {
+    color: '#D9D8DB'
+  },
+  aboutas: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: '#B9B9BA',
+    boxShadow: '1px 1px 3px 1px #D9D8DB'
   }
+  //nitin
+  // main: {
+  //   margin: theme.spacing() * 1
+  // },
+  // about: {
+  //   margin: theme.spacing() * 1
+  // },
+
+  // btn: {
+  //   backgroundColor: theme.palette.primary.main
+  // },
+  // CardMedia: {
+  //   margin: theme.spacing() * 2,
+  //   width: theme.spacing() * 6
+  // }
 });
 
 export default style;
