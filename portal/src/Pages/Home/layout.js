a1bab3b92f1309410916f1abf3c20e01e2758be7
sanjay1906 <sanjaythakur1906@gmail.com>
2019-10-02 13:32:02 +0530

    testimonials

--- portal/src/Pages/Home/layout.js
+++ portal/src/Pages/Home/layout.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
-import { withStyles, Typography } from '@material-ui/core';
-
+import {
+  withStyles,
+  Typography,
+  CardContent,
+  Card,
+  Grid,
+  Container,
+  Avatar
+} from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
 import style from './style';
 import { Header } from 'Components';
+
+const cards = [1, 2, 3];
+
+const user = {
+  name: 'Sanjay Jadav',
+  avatar: 'download.jpeg',
+  bio: 'Venom'
+};
 
 class Layout extends Component {
   render() {
@@ -11,6 +27,36 @@ class Layout extends Component {
       <div className={classes.container}>
         <Header />
         <Typography variant='body1'>Home Screen</Typography>
+        <Container className={classes.cardGrid} maxWidth='md'>
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <Avatar
+                    alt='Person'
+                    className={classes.avatar}
+                    component={RouterLink}
+                    src={user.avatar}
+                    // to='/settings'
+                  />
+                  <Typography className={classes.name} variant='h5'>
+                    {user.name}
+                  </Typography>
+                  <Typography className={classes.specification} variant='1px'>
+                    {user.bio}
+                  </Typography>
+                  <CardContent className={classes.cardContent}>
+                    <Typography className={classes.aboutas}>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </div>
     );
   }
--- portal/src/Pages/Home/style.js
+++ portal/src/Pages/Home/style.js
@@ -3,7 +3,43 @@ const style = theme => ({
     display: 'flex',
     alignItems: 'center',
     flex: 1,
-    flexDirection: 'column'
+    flexDirection: 'column',
+    color: '#FAFCFE'
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#F8F8F8',
+    boxShadow: 'none'
+  },
+  cardMedia: {
+    paddingTop: '56.25%'
+  },
+  cardContent: {
+    flexGrow: 1
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: '50'
+  },
+  name: {
+    marginTop: theme.spacing(1)
+  },
+  specification: {
+    color: '#D9D8DB'
+  },
+  aboutas: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: '#B9B9BA',
+    boxShadow: '1px 1px 3px 1px #D9D8DB'
   }
 });
 
